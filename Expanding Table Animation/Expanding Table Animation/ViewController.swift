ccfe784288e2b144069a1daa32b1e482bdf3f578
dennismarks <dennismarkshome@gmail.com>
2019-05-30 18:48:57 -0400

    add main view controller

--- Expanding Table Animation/Expanding Table Animation/ViewController.swift
+++ Expanding Table Animation/Expanding Table Animation/ViewController.swift
@@ -20,6 +20,50 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         self.collectionView.dataSource = self
     }
     
+    var openingFrame: CGRect?
     
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let presentationAnimator = ExpandAnimator.animator
+        presentationAnimator.openingFrame = openingFrame!
+        presentationAnimator.transitionMode = .Present
+        return presentationAnimator
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let presentationAnimator = ExpandAnimator.animator
+        presentationAnimator.openingFrame = openingFrame!
+        presentationAnimator.transitionMode = .Dismiss
+        return presentationAnimator
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colourArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.width, height: 150)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let colour = colourArray[indexPath.row]
+        cell.backgroundColor = UIColor.init(named: colour)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Set frame of cell
+        let attributes = collectionView.layoutAttributesForItem(at: indexPath)
+        let attributesFrame = attributes?.frame
+        let frameToOpenFrom = collectionView.convert(attributesFrame!, to: collectionView.superview)
+        openingFrame = frameToOpenFrom
+        
+        
+    }
     
 }
